d494f7e2510d9d575d08580edad3bce6ee9af7ed
hariharane01 <hariharane01@gmail.com>
2024-03-20 22:48:18 +0530

    to create login

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv"); //use to get dotenv file 
 dotenv.config();
+
+const authRoute = require("./routes/auth"); //creating router for auth file 
 
 const MONGOURL = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 
+//to connect MongoDB we using mongoose 
 mongoose
   .connect(MONGOURL)
   .then(() => console.log("Database Connection Successfull"))
   .catch((err) => {
     console.error(err);
   });
+
+app.use(express.json());
+app.use("/api/auth", authRoute); //route for authenthication 
+
 
 app.listen(9000, () => {
   console.log("server is connected");
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,52 @@
-const routerr = require("express").Router();
+const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //Register
-routerr.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const newUser = new User({
+    //creating new user
     username: req.body.username,
+    email: req.body.email,
+
+    //to encrypt the password
+    password: CryptoJS.AES.encrypt(
+      //use in crypto js for encrypt for password
+      req.body.password,
+      process.env.KEY
+    ).toString(),
   });
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    //to check the user
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    !user && res.status(401).json("Wrong password or username! ");
+
+    //to decrypt the password
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    
+    //to check the password
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or usetname!");
+    //to store the password   
+    const { password, ...info } = user._doc;
+
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
